b37b2b6750e12b251b2edf7e4738201a7c3575a6
DvpSun <dvpsun@gmail.com>
2025-11-11 17:03:17 +0300

    upd: rational test

---
+++ prj.test/rational_test.cpp
@@ -0,0 +1,22 @@
+#include <rational/rational.hpp>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("[rationa] - ctor default") {
+  Rational r0;
+  CHECK(0 == r0.num());
+  CHECK(1 == r0.den());
+}
+
+TEST_CASE("[rationa] - ctor") {
+  Rational r1(3);
+  CHECK(3 == r1.num());
+  CHECK(1 == r1.den());
+  Rational r2(-6, 2);
+  CHECK(-3 == r2.num());
+  CHECK(1 == r2.den());
+  Rational r3(2, -6);
+  CHECK(-1 == r2.num());
+  CHECK(3 == r2.den());
+}
